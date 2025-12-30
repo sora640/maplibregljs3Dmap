8f35dd1c4f976317ff89b15dfc6eb988b164d690
sora640 <indigo.comet11@gmail.com>
2025-12-31 03:51:42 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const map = new maplibregl.Map({
   container: "map",
-  center: [139.7024, 35.6598], // 中心座標
-  zoom: 16, // ズームレベル
+  center: [134.310679, 34.47023], // 中心座標
+  zoom: 10, // ズームレベル
   style: {
     // スタイル仕様のバージョン番号。8を指定する
     version: 8,
